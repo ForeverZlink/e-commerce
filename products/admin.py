4532f56ff52d1d9ff46a035c19a44a8182644c95
St;ZlinkG <carloscunha080@gmail.com>
2021-10-14 21:03:38 -0300

    Criei os models necessários para o cadastro de um produto e adicionei eles na página de administração do django para que o admin possa cadastrar seus produtos. Também criei o app

---
+++ products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.db import models
+from products.models import Product, Type_of_Product, Mark_Product, Imagem_Product
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Type_of_Product)
+class TypeProductAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Mark_Product)
+class MarkProductAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Imagem_Product)
+class ImagemProductAdmin(admin.ModelAdmin):
+    pass
