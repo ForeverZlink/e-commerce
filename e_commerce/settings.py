0f64ef7bf076f56a4e7da6a0170b3729d87b0d95
St;ZlinkG <carloscunha080@gmail.com>
2021-10-14 21:19:27 -0300

    Criei os models necessários para o cadastro de um produto e adicionei eles na página de administração do django para que o admin possa cadastrar seus produtos. Também criei o app, adicionei o Media Root(o local para onde vair os imagems ou algo do genêro)

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 STATIC_URL = '/static/'
 
