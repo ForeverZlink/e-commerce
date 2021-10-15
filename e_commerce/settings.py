74b7e5e48ab725411cb37e4d439fbc2e2a0b3474
St;ZlinkG <carloscunha080@gmail.com>
2021-10-14 22:29:40 -0300

    Adicionei o django heroku no projeto

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +126,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+
+django_heroku.settings(locals())
