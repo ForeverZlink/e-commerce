546d7b3d658a3b0833550378f46e8d8e033665fe
St;ZlinkG <carloscunha080@gmail.com>
2021-11-24 21:14:55 -0300

    Criei um test para view de new_order. Foi a partir desse teste que eu desenvolvi a lógica da view

--- orders/tests.py
+++ orders/tests.py
@@ -1,7 +1,35 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from orders.models import Orders
+from products.models import *
 from django.shortcuts import reverse
 # Create your tests here.
 class TestViews(TestCase):
+    @classmethod
+    def setUp(self) :
+        self.user             = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        self.user.save()
+        self.type_product     =Type_of_Product.objects.create(type_product="SmartPhone",public_target="Everybody")
+        self.mark_product     =Mark_Product.objects.create(name_of_mark="Samsumg",
+                description_of_mark="A mark of inovation")
+        self.product=Product.objects.create(
+            type_of_product=self.type_product,
+            mark_of_product=self.mark_product,
+            name='Galaxy A30',
+            price="1000",
+            descripition="A good cellphone",
+            inventory=5,
+            
+        )
+        self.product.save()
+
     def test_create_new_order(self):
-        self.client.get(reverse(''))
+        self.client.login(username='john',password='johnpassword')
+
+        path  = 'orders:new_order'
+        response=self.client.get(reverse(path,args=(self.product,)))
+        self.assertEqual(response.status_code,302)
+        order = Orders.objects.all()[0]
+        
+        self.assertEqual(order.product.name,"Galaxy A30")
--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,21 @@
 from django.db.models.query import RawQuerySet
-from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseNotModified, HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.contrib.auth.models import User
 from orders.models import ShoppingCart, Orders
+from products.models import Product
 
 # Create your views here.
 def new_order(request,pk_product):
-    user    = request.user
+    
+    product=Product.objects.get(pk=pk_product)
+    user_pk  = request.user.pk
+    
+    user_instance = User.objects.get(pk=user_pk)
     order   = Orders.objects.create(
-                user = user , 
-                product=pk_product
+                user = user_instance , 
+                product=product
             )
     order.save()
+    return HttpResponseRedirect(reverse('products:home_page'))
     
