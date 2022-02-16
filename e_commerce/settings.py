c29bd379a4d4dcbf364d3437a9fcaba204a209f6
St;ZlinkG <carloscunha080@gmail.com>
2022-02-15 21:11:19 -0300

    Agora a secret_key do projeto é buscada como variável de ambiente. Eu já coloquei a variável no site que estou hospedando

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c0&i-m7iz&_sd)60hea%ux06q$an7r_e(ui2dj=mp!_%u5_6*a'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
